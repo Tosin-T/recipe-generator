d86290a32bba07e27707333a5fe70f59d1435f37
tosin-t <tosin100@homail.com>
2023-11-30 19:48:14 +0000

    test download

--- Assets/javaScript/recipe.js
+++ Assets/javaScript/recipe.js
@@ -1,11 +1,19 @@
-var x="https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
+var x="https://www.themealdb.com/api/json/v1/1/search.php?s=beans"
 var y = "https://www.themealdb.com/api/json/v1/1/random.php"
+var searchBar= document.getElementById("searchBar")
 function formInput(){
     
-    fetch(y)
+    fetch(x)
     .then(function(response){
     return response.json();
     }).then(function(data){
         console.log(data)})
     }
     formInput()
+
+function getInput(event){
+    event.preventDefault
+    var q=searchBar.value
+    console.log(q)
+}
+    
