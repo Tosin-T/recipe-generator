65f4615288afd8d6720b4a8262867b790d1727b1
Orange Workbook <d.riley0189@gmail.com>
2023-12-05 11:25:33 +0000

    skeleton logic for filter function

--- Assets/javaScript/recipe.js
+++ Assets/javaScript/recipe.js
@@ -1,14 +1,42 @@
-var specific ="https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
-var random = "https://www.themealdb.com/api/json/v1/1/random.php"
-var categories = "https://www.themealdb.com/api/json/v1/1/categories.php"
+var buttonSearchMain = document.getElementById('main-btn');
+var buttonSearchSecondary = document.getElementById('secondary-btn');
+var mainSeachBar = document.getElementById('main-search');
+
+var userSearchMain = "";
+var userSearchSecondary = "";
 
 
-function formInput(){
+var apiURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+var randomURL = "https://www.themealdb.com/api/json/v1/1/random.php";
+var queryURL;
+var categoriesURL = "https://www.themealdb.com/api/json/v1/1/categories.php";
+
+
+
+//MAIN SEARCH BUTTON.
+
+buttonSearchMain.addEventListener('click', function () {
+
+    fetch(categoriesURL)
+            .then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                console.log(data);})
+
+});
+
+//SEARCH BUTTON ON THE TOP RIGHT HAND CORNER.
+
+buttonSearchSecondary.addEventListener('click', function () {
+
     
-    fetch(categories)
-    .then(function(response){
-    return response.json();
-    }).then(function(data){
-        console.log(data)})
-    }
-    formInput()
+    console.log('secondary button works');
+    var userSearchSecondary = document.getElementById("secondaryInput").value;
+    console.log(userSearchSecondary);
+
+
+
+
+
+});
+
---
+++ Assets/javaScript/removeByName.js
@@ -0,0 +1,22 @@
+let arr = ["apple", "banana", "orange"];
+
+
+const removeByValue = (value) => {
+    let indexOf = arr.indexOf(value);
+
+    for(i=0; i<arr.length; i++) {
+        if(value == arr[i]) {
+            console.log(`value: ${value} arr[i]: ${arr[i]}`)
+            let spliceByIndex = arr.splice(indexOf, 1);
+            console.log(`Spliced value: ${spliceByIndex}`)
+        }
+    }
+    
+    
+
+    return arr + console.log(`Array values: ${arr}`)
+    
+    
+}
+
+removeByValue("bana ")
