af5b4aafa81e7b343ae6802c1307a50274f3eae9
Orange Workbook <d.riley0189@gmail.com>
2023-12-05 12:28:59 +0000

    adds hard-coded checkbox functionality

--- Assets/javaScript/recipe.js
+++ Assets/javaScript/recipe.js
@@ -5,11 +5,44 @@ var mainSeachBar = document.getElementById('main-search');
 var userSearchMain = "";
 var userSearchSecondary = "";
 
-
-var apiURL = "https://www.themealdb.com/api/json/v1/1/search.php?s="
-var y = "https://www.themealdb.com/api/json/v1/1/random.php"
+//URL Minus dynamically generated SearchWord for recipe
+var apiURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+//user Search to be added to apiURL
 var queryURL;
 
+//random API 
+var randomURL = "https://www.themealdb.com/api/json/v1/1/random.php";
+var categoriesURL = "https://www.themealdb.com/api/json/v1/1/categories.php";
+
+//-----------------------------------------------------------------------------------------------------------
+
+//Checkbox filters
+
+    let checkVegetarian = document.getElementById("vegetarian");
+    let checkVegan = document.getElementById("vegan");
+
+    let arrOfCategories = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+    //console.log(checkVegetarian.value)
+
+    checkVegetarian.addEventListener('click', function () {
+        fetch(categoriesURL)
+            .then(function (response) {
+                return response.json();
+
+            }).then(function (data) {
+                //console.log(data.categories);
+                if(checkVegetarian.value == "true") {
+                    checkVegetarian.value = "false";
+                    console.log(`check is now false!`)
+
+                } else if (checkVegetarian.value == "false") {
+                    checkVegetarian.value = "true";
+                    console.log(`check is now true!`)
+
+                }
+            })
+        })
 
 
 //MAIN SEARCH BUTTON.
@@ -30,60 +63,49 @@ buttonSearchMain.addEventListener('click', function () {
     queryURL = apiURL + userSearchMain;
 
 
+    // fetch(queryURL)
+    //     .then(function (response) {
+    //         return response.json();
+    //     }).then(function (data) {
 
-    fetch(queryURL)
-        .then(function (response) {
-            return response.json();
-        }).then(function (data) {
+    //         //Function to pull a different recipe if more than 1 is returned from API.
+    //         function getRandomArray(array) {
+    //             for (i = 0; i < data.meals.length; i++) {
+    //                 var randomArray = Math.floor(Math.random() * array.length);
+    //                 return array[randomArray];
+    //             }
+    //         }
 
-            //Function to pull a different recipe if more than 1 is returned from API.
-            function getRandomArray(array) {
-                for (i = 0; i < data.meals.length; i++) {
-                    var randomArray = Math.floor(Math.random() * array.length);
-                    return array[randomArray];
-                }
-            }
-
-            var dish = getRandomArray(data.meals);
-
-
-            document.getElementById("instructions").textContent = dish.strInstructions;;
-            document.getElementById("recipe-name").textContent = dish.strMeal;
-            document.getElementById("image").setAttribute('src', dish.strMealThumb);
+    //         var dish = getRandomArray(data.meals);
 
 
-            //Function to get ingredients.
-            var ingredientsArray = Object.keys(dish)
-                .filter(key => key.startsWith('strIngredient'))
-                .map(key => dish[key])
-                .filter(ingredient => ingredient.trim() !== '');
-            console.log(ingredientsArray);
-
-            for (var i = 0; i < ingredientsArray.length; i++) {
-
-                var ListEl = document.querySelector('#ingredients');
-
-                var ingredient = ingredientsArray[i];
+    //         document.getElementById("instructions").textContent = dish.strInstructions;;
+    //         document.getElementById("recipe-name").textContent = dish.strMeal;
+    //         document.getElementById("image").setAttribute('src', dish.strMealThumb);
 
 
-                var li = document.createElement("li");
-                li.textContent = ingredient;
-                li.setAttribute('class', 'ingredients');
-                ListEl.appendChild(li);
+    //         //Function to get ingredients.
+    //         var ingredientsArray = Object.keys(dish)
+    //             .filter(key => key.startsWith('strIngredient'))
+    //             .map(key => dish[key])
+    //             .filter(ingredient => ingredient.trim() !== '');
+    //         console.log(ingredientsArray);
+
+    //         for (var i = 0; i < ingredientsArray.length; i++) {
+
+    //             var ListEl = document.querySelector('#ingredients');
+
+    //             var ingredient = ingredientsArray[i];
 
 
-            }
+    //             var li = document.createElement("li");
+    //             li.textContent = ingredient;
+    //             li.setAttribute('class', 'ingredients');
+    //             ListEl.appendChild(li);
 
+    //         }
 
-
-
-
-
-        })
-
-
-
-
+    //     })
 
 });
 
--- Assets/javaScript/removeByName.js
+++ Assets/javaScript/removeByName.js
@@ -4,7 +4,7 @@ let arr = ["apple", "banana", "orange"];
 const removeByValue = (value) => {
     let indexOf = arr.indexOf(value);
 
-    for(i=0; i<arr.length; i++) {
+    for(i=0; i < arr.length; i++) {
         if(value == arr[i]) {
             console.log(`value: ${value} arr[i]: ${arr[i]}`)
             let spliceByIndex = arr.splice(indexOf, 1);
@@ -19,4 +19,4 @@ const removeByValue = (value) => {
     
 }
 
-removeByValue("bana ")
+removeByValue("banana")
