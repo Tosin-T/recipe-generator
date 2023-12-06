4285321a55283c111d5d53db45757f558705ad18
Orange Workbook <d.riley0189@gmail.com>
2023-12-06 16:11:34 +0000

    tabbed over script.js correctly

--- Assets/javaScript/script.js
+++ Assets/javaScript/script.js
@@ -35,121 +35,135 @@ const uncheckAll = () => {
 
 //Checkbox filters
 
-    let checkVegetarian = document.getElementById("vegetarian");
-    let checkVegan = document.getElementById("vegan");
+let checkVegetarian = document.getElementById("vegetarian");
+let checkVegan = document.getElementById("vegan");
 
-    let categoriesArr = [
-                            "Beef", 
-                            "Chicken", 
-                            "Dessert", 
-                            "Lamb", 
-                            "Miscellaneous", 
-                            "Pasta",
-                            "Pork",
-                            "SeaFood",
-                            "Side",
-                            "Starter",
-                            "Vegan",
-                            "Vegetarian",
-                            "Breakfast",
-                            "Goat"
-                        ];
+let categoriesArr = [
+                        "Beef", 
+                        "Chicken", 
+                        "Dessert", 
+                        "Lamb", 
+                        "Miscellaneous", 
+                        "Pasta",
+                        "Pork",
+                        "SeaFood",
+                        "Side",
+                        "Starter",
+                        "Vegan",
+                        "Vegetarian",
+                        "Breakfast",
+                        "Goat"
+                    ];
 
-    let chosenCategories = [];
-    let spliceByIndex;
+let chosenCategories = [];
+let spliceByIndex;
 
-    //function to remove an element from an array based on its name
-    const removeByValue = (arr, value) => {
-        let indexOf = arr.indexOf(value);
-    
-        for(i=0; i<arr.length; i++) {
-            if(value == arr[i]) {
-                //console.log(`value: ${value} arr[i]: ${arr[i]}`);
-                spliceByIndex = arr.splice(indexOf, 1);
-                //console.log(`Spliced value: ${spliceByIndex}`);
+//function to remove an element from an array based on its name
+const removeByValue = (arr, value) => {
+    let indexOf = arr.indexOf(value);
 
-            }
-        }
-        
-        //console.log(`Array values: ${arr}`);
-        return arr   
-    };
-
-    //function to add an element to an array
-    const addByValue = (arr, value) => {
-    
-        //checking value is not already in array before pushing
-        if(arr.includes(value) != true) {
-            arr.push(value);
-            //console.log(`Pushed value: ${value}`);
+    for(i=0; i<arr.length; i++) {
+        if(value == arr[i]) {
+            //console.log(`value: ${value} arr[i]: ${arr[i]}`);
+            spliceByIndex = arr.splice(indexOf, 1);
+            //console.log(`Spliced value: ${spliceByIndex}`);
 
         }
+    }
+    
+    //console.log(`Array values: ${arr}`);
+    return arr   
+};
 
-        //removes element from array of chosen categories
-        removeByValue(chosenCategories, value);
+//function to add an element to an array
+const addByValue = (arr, value) => {
+
+    //checking value is not already in array before pushing
+    if(arr.includes(value) != true) {
+        arr.push(value);
+        //console.log(`Pushed value: ${value}`);
+
+    }
+
+    //removes element from array of chosen categories
+    removeByValue(chosenCategories, value);
+    
+
+    //console.log(`Your chosen categories are now: ${chosenCategories}`);
+    //console.log(`Array values: ${arr}`);
+    return arr   
+};
+
+//--------------------------------------EVENT LISTENERS------------------------------------------------
+
+//TODO: SAVE ALL CHECKBOXES TO VARIABLE. EVENT LISTENER SHOULD USE IF STATEMENTS TO SELECT CORRECT ID.
+
+//checkbox eventListener
+//Vegetarian only. Not DRY but for MVP purposes.
+checkVegetarian.addEventListener('click', function () {
+
+    if(checkVegetarian.value == "true") {
+        checkVegetarian.value = "false";
+        console.log(`check is now false!`);
+
+        //function call to add value to array
+        addByValue(categoriesArr, "Vegetarian");
         
 
-        //console.log(`Your chosen categories are now: ${chosenCategories}`);
-        //console.log(`Array values: ${arr}`);
-        return arr   
-    };
+    } else if (checkVegetarian.value == "false") {
+        checkVegetarian.value = "true";
+        console.log(`check is now true!`);
 
-    //--------------------------------------EVENT LISTENERS------------------------------------------------
+        //function call to remove value from array
+        removeByValue(categoriesArr, "Vegetarian");
 
-    //TODO: SAVE ALL CHECKBOXES TO VARIABLE. EVENT LISTENER SHOULD USE IF STATEMENTS TO SELECT CORRECT ID.
-    const checkboxes = document.querySelectorAll("checkbox");
-    console.log(checkboxes)
+        chosenCategories.push(spliceByIndex);
+        console.log(`Your chosen categories are now: ${chosenCategories}`);
 
-    //checkbox eventListener
-    checkVegetarian.addEventListener('click', function () {
+    }
+});
 
-        // fetch(lookUpURL)
-        //     .then(function (response) {
-        //         return response.json();
+//Vegan only. Not DRY but for MVP purposes.
+checkVegan.addEventListener('click', function () {
 
-        //     }).then(function (data) {
-        //         console.log(data)
-                //console.log(data.categories);
-                if(checkVegetarian.value == "true") {
-                    checkVegetarian.value = "false";
-                    console.log(`check is now false!`);
+    if(checkVegan.value == "true") {
+        checkVegan.value = "false";
+        console.log(`check is now false!`);
 
-                    //function call to add value to array
-                    addByValue(categoriesArr, "Vegetarian");
+        //function call to add value to array
+        addByValue(categoriesArr, "Vegan");
 
-                    
+    } else if (checkVegan.value == "false") {
+        checkVegan.value = "true";
+        console.log(`check is now true!`);
 
-                } else if (checkVegetarian.value == "false") {
-                    checkVegetarian.value = "true";
-                    console.log(`check is now true!`);
+        //function call to remove value from array
+        removeByValue(categoriesArr, "Vegan");
 
-                    //function call to remove value from array
-                    removeByValue(categoriesArr, "Vegetarian");
+        chosenCategories.push(spliceByIndex);
+        console.log(`Your chosen categories are now: ${chosenCategories}`);
 
-                    chosenCategories.push(spliceByIndex);
-                    console.log(`Your chosen categories are now: ${chosenCategories}`);
+    }
+});
 
-                }
-            })
-        //});
 
 
 //MAIN SEARCH BUTTON.
 
 buttonSearchMain.addEventListener('click', function () {
 
-    document.getElementById('main-content').setAttribute('class', 'show');
+document.getElementById('main-content').setAttribute('class', 'show');
 
-    //console.log('main button works');
-    mainSeachBar.setAttribute('class', 'hide');
-    var userSearchMain = document.getElementById('mainInput').value;
-    //console.log(userSearchMain);
+//console.log('main button works');
+mainSeachBar.setAttribute('class', 'hide');
+var userSearchMain = document.getElementById('mainInput').value;
+//console.log(userSearchMain);
 
-    if (userSearchMain == '') {
+if (userSearchMain == '') {
 
-    }
+}
 
-    queryURL = apiURL + userSearchMain;
+queryURL = apiURL + userSearchMain;
 
 
 fetch(queryURL)
