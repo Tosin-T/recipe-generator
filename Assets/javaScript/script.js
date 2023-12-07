78563b9f5f6a4e5442f25a93844273f3ceb342a2
Orange Workbook <d.riley0189@gmail.com>
2023-12-07 22:07:21 +0000

    merged filter feature with main

--- Assets/javaScript/script.js
+++ Assets/javaScript/script.js
@@ -1,19 +1,17 @@
 var buttonSearchMain = document.getElementById('main-btn');
-var buttonSearchSecondary = document.getElementById('secondary-btn');
+var backButton = document.getElementById('back-btn');
 var mainSeachBar = document.getElementById('main-search');
-
+var favouriteButton = document.getElementById("favouriteButton")
 var userSearchMain = "";
 var userSearchSecondary = "";
 
-//URL Minus dynamically generated SearchWord for recipe
-var apiURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
-//user Search to be added to apiURL
+
+
+
+var apiURL = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+var y = "https://www.themealdb.com/api/json/v1/1/random.php"
 var queryURL;
 
-//random API 
-var randomURL = "https://www.themealdb.com/api/json/v1/1/random.php";
-var categoriesURL = "https://www.themealdb.com/api/json/v1/1/categories.php";
-var lookUpURL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772";
 
 //-----------------------------------------------------------------------------------------------------------
 
@@ -42,8 +40,6 @@ let checkMeatDishes = document.getElementById("meatDishes");
 let dishType = "allDishes";
 
 //--------------------------------------EVENT LISTENERS------------------------------------------------
-
-//TODO: SAVE ALL CHECKBOXES TO VARIABLE. EVENT LISTENER SHOULD USE IF STATEMENTS TO SELECT CORRECT ID.
 
 //checkbox eventListener
 
@@ -93,103 +89,231 @@ checkMeatDishes.addEventListener('click', function () {
 
 
 
-//-----------------------MAIN SEARCH BUTTON--------------------------------------------------------------------------
-buttonSearchMain.addEventListener('click', function () {
-console.log("I got clicked")
+//MAIN SEARCH BUTTON.
 
-document.getElementById('main-content').setAttribute('class', 'show');
-
-//console.log('main button works');
-mainSeachBar.setAttribute('class', 'hide');
-var userSearchMain = document.getElementById('mainInput').value;
-//console.log(userSearchMain);
-
-if (userSearchMain == '') {
-
-}
-
-queryURL = apiURL + userSearchMain;
+buttonSearchMain.addEventListener('click', PresentContent);
 
 
-fetch(queryURL)
-    .then(function (response) {
-        return response.json();
+    function PresentContent() {
 
-    }).then(function (data) {
-        // console.log(data)
-
-        let mealsData = data.meals;
-
-        if (dishType == "allDishes") {
-            console.log("all dishes selected");
-    
-        } else if (dishType == "Vegetarian") {
-            console.log("Vegetarian Selected.");
-            mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan");
-
-        } else if (dishType == "MeatDishes") {
-            console.log("Meat Dishes Selected");
-            mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat" || foodCategory.strCategory === "Seafood"); 
-
-        } else {
-            console.log("Error: check conditionals");
-        }
-
-
-        function getRandomArray(array) {
-            for (i = 0; i < data.meals.length; i++) {
-
-                var randomArray = Math.floor(Math.random() * array.length);
-                return array[randomArray];
-            }
-        }
+        document.getElementById('back-btn').setAttribute('class', 'show');
         
-        var dish = getRandomArray(mealsData);
-        console.log(mealsData)
+    
+    document.getElementById('main-content').setAttribute('class', 'show');
 
-        document.getElementById("instructions").textContent = dish.strInstructions;;
-        document.getElementById("recipe-name").textContent = dish.strMeal;
-        document.getElementById("image").setAttribute('src', dish.strMealThumb);
+    console.log('main button works');
+    mainSeachBar.setAttribute('class', 'hide');
+    var userSearchMain = document.getElementById('mainInput').value;
+    console.log(userSearchMain);
+
+    
+
+    queryURL = apiURL + userSearchMain;
 
 
-        //Function to get ingredients.
-        var ingredientsArray = Object.keys(dish)
-            .filter(key => key.startsWith('strIngredient'))
+
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+
+        }).then(function (data) {
+            //console.log(data)
+            let mealsData = data.meals;
+
+            if (dishType == "allDishes") {
+                console.log("all dishes selected");
+
+            } else if (dishType == "Vegetarian") {
+                console.log("Vegetarian Selected.");
+                mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan");
+
+            } else if (dishType == "MeatDishes") {
+                console.log("Meat Dishes Selected");
+                mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat" || foodCategory.strCategory === "Seafood"); 
+
+            } else {
+                console.log("Error: check conditionals");
+            }
+
+
+
+            //Function to pull a different recipe if more than 1 is returned from API.
+            function getRandomArray(array) {
+                for (i = 0; i < data.meals.length; i++) {
+                    var randomArray = Math.floor(Math.random() * array.length);
+                    return array[randomArray];
+                }
+            }
+
+            var dish = getRandomArray(mealsData);
+            console.log(mealsData);
+
+
+            document.getElementById("instructions").textContent = dish.strInstructions;;
+            RandomMealname = document.getElementById("recipe-name").textContent = dish.strMeal;
+            document.getElementById("image").setAttribute('src', dish.strMealThumb);
+
+
+            //Function to get ingredients.
+            var ingredientsArray = Object.keys(dish)
+                .filter(key => key.startsWith('strIngredient'))
+                .map(key => dish[key])
+                .filter(ingredient => ingredient !== '')
+                .filter(ingredient => ingredient !== null);
+            
+
+            for (var i = 0; i < ingredientsArray.length; i++) {
+
+                var ListEl = document.querySelector('#ingredients');
+
+                var ingredient = ingredientsArray[i];
+
+
+                var li = document.createElement("li");
+                li.textContent = ingredient;
+                li.setAttribute('class', 'ingredients');
+                ListEl.appendChild(li);
+
+
+            }
+
+            //Function to get measures.
+            var measureArray = Object.keys(dish)
+            .filter(key => key.startsWith('strMeasure'))
             .map(key => dish[key])
-            .filter(ingredient => ingredient.trim() !== '');
-        console.log(ingredientsArray);
-
-        for (var i = 0; i < ingredientsArray.length; i++) {
-
-            var ListEl = document.querySelector('#ingredients');
-
-            var ingredient = ingredientsArray[i];
+            .filter(measure =>measure !=='')
+            .filter(measure => measure !== null);
 
 
-            var li = document.createElement("li");
-            li.textContent = ingredient;
-            li.setAttribute('class', 'ingredients');
-            ListEl.appendChild(li);
+            for (var x = 0; x < measureArray.length; x++) {
+                
+                var ListElementMeasure =document.querySelector ('#measure');
+                var measure = measureArray[x];
+                var li2 = document.createElement("li");
+                li2.textContent = measure;
+                li2.setAttribute('class', 'measure');
+                ListElementMeasure.appendChild(li2);
 
-        }
+            }
 
-    })
+        })
 
-});
+}; 
+
 
 
 //SEARCH BUTTON ON THE TOP RIGHT HAND CORNER.
 
-buttonSearchSecondary.addEventListener('click', function () {
+backButton.addEventListener('click', function () {
+
+    window.location.reload();
+});
+
+// create button feature
+var favouriteRecipe = []
+var favouriteRecipeListID = document.getElementById("favoritesList")
+
+function renderFavouriteRecipeButton() {
+    favouriteRecipeListID.innerHTML = ''
+
+    for (i = 0; i < favouriteRecipe.length; i++) {
+        var favouriteButton = document.createElement("button")
+        favouriteButton.classList.add("recipies")
+        favouriteButton.setAttribute("data-recipeName", favouriteRecipe[i]);
+        favouriteButton.textContent = favouriteRecipe[i]
+        favouriteRecipeListID.append(favouriteButton)
+
+    }
+}
+// buttonSearchMain.addEventListener("click",test )
+favouriteButton.addEventListener("click", addingRecipieToArray1)
 
 
-    console.log('secondary button works');
-    var userSearchSecondary = document.getElementById("secondaryInput").value;
-    console.log(userSearchSecondary);
+// buttonSearchMain.addEventListener('click', renderFavouriteRecipeButton)
+
+function addingRecipieToArray1() {
+    favouriteRecipe.push(RandomMealname)
+    renderFavouriteRecipeButton();
+    console.log(favouriteRecipe)
+}
+function addingRecipieToArray2() {
+    var userSearchSecondary = secondaryInput.value;
+
+    favouriteRecipe.push(userSearchSecondary)
+    console.log(userSearchSecondary)
+    renderFavouriteRecipeButton();
+    console.log(favouriteRecipe)
+    }
+
+    // GET RANDOM MEAL BUTTON
+
+var getRandomMeal = document.getElementById("get-meal");
+
+getRandomMeal.addEventListener('click', function (event) {
+    event.preventDefault();
+    mainSeachBar.setAttribute('class', 'hide');
+
+
+    document.getElementById('main-content').setAttribute('class', 'show');
+    fetch(y)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+
+            var dish = data.meals[0];
+
+            console.log(dish);
+            document.getElementById("instructions").textContent = dish.strInstructions;
+            document.getElementById("recipe-name").textContent = dish.strMeal;
+            document.getElementById("image").setAttribute('src', dish.strMealThumb);
+            //Function to get ingredients.
+            var ingredientsArray = Object.keys(dish)
+                .filter(key => key.startsWith('strIngredient'))
+                .map(key => dish[key])
+                .filter(ingredient => ingredient !== '')
+                .filter(ingredient => ingredient !== null);
+            
+
+            for (var i = 0; i < ingredientsArray.length; i++) {
+
+                var ListEl = document.querySelector('#ingredients');
+
+                var ingredient = ingredientsArray[i];
+
+
+                var li = document.createElement("li");
+                li.textContent = ingredient;
+                li.setAttribute('class', 'ingredients');
+                ListEl.appendChild(li);
+
+
+            }
+
+            //Function to get measures.
+            var measureArray = Object.keys(dish)
+            .filter(key => key.startsWith('strMeasure'))
+            .map(key => dish[key])
+            .filter(measure =>measure !=='')
+            .filter(measure => measure !== null);
+
+
+            for (var x = 0; x < measureArray.length; x++) {
+                
+                var ListElementMeasure =document.querySelector ('#measure');
+                var measure = measureArray[x];
+                var li2 = document.createElement("li");
+                li2.textContent = measure;
+                li2.setAttribute('class', 'measure');
+                ListElementMeasure.appendChild(li2);
 
 
 
 
+            }
 
+
+        
+            
+        })
 });
 
