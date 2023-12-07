509751eee5d1b7c0b50db242d60a44774d24e8b1
Orange Workbook <d.riley0189@gmail.com>
2023-12-07 20:56:47 +0000

    removes commented out script.js code

--- Assets/javaScript/script.js
+++ Assets/javaScript/script.js
@@ -38,61 +38,6 @@ let allDishes = document.getElementById("allDishes");
 let checkVegetarian = document.getElementById("vegetarian");
 let checkMeatDishes = document.getElementById("meatDishes");
 
-// let categoriesArr = [
-//                         "Beef", 
-//                         "Chicken", 
-//                         "Dessert", 
-//                         "Lamb", 
-//                         "Miscellaneous", 
-//                         "Pasta",
-//                         "Pork",
-//                         "SeaFood",
-//                         "Side",
-//                         "Starter",
-//                         "Seafood",
-//                         "Vegetarian",
-//                         "Breakfast",
-//                         "Goat"
-//                     ];
-
-// let chosenCategories = [];
-// let spliceByIndex;
-
-// //function to remove an element from an array based on its name
-// const removeByValue = (arr, value) => {
-//     let indexOf = arr.indexOf(value);
-
-//     for(i=0; i<arr.length; i++) {
-//         if(value == arr[i]) {
-//             //console.log(`value: ${value} arr[i]: ${arr[i]}`);
-//             spliceByIndex = arr.splice(indexOf, 1);
-//             //console.log(`Spliced value: ${spliceByIndex}`);
-
-//         }
-//     }
-    
-//     //console.log(`Array values: ${arr}`);
-//     return arr   
-// };
-
-// //function to add an element to an array
-// const addByValue = (arr, value) => {
-
-//     //checking value is not already in array before pushing
-//     if(arr.includes(value) != true) {
-//         arr.push(value);
-//         //console.log(`Pushed value: ${value}`);
-
-//     }
-
-//     //removes element from array of chosen categories
-//     removeByValue(chosenCategories, value);
-    
-
-//     //console.log(`Your chosen categories are now: ${chosenCategories}`);
-//     //console.log(`Array values: ${arr}`);
-//     return arr   
-// };
 
 let dishType = "allDishes";
 
@@ -113,7 +58,6 @@ allDishes.addEventListener('click', function (event) {
         
         dishType = "allDishes";
         console.log(`dishType: ${dishType}`);
-
     }
 });
 
@@ -129,23 +73,6 @@ checkVegetarian.addEventListener('click', function (event) {
         
         dishType = "Vegetarian";
         console.log(`dishType: ${dishType}`);
-    //     checkVegetarian.value = "false";
-    //     console.log(`check is now false!`);
-
-    //     //function call to add value to array
-    //     addByValue(categoriesArr, "Vegetarian");
-        
-
-    // } else if (checkVegetarian.value == "false") {
-    //     checkVegetarian.value = "true";
-    //     console.log(`check is now true!`);
-
-    //     //function call to remove value from array
-    //     removeByValue(categoriesArr, "Vegetarian");
-
-    //     chosenCategories.push(spliceByIndex);
-    //     console.log(`Your chosen categories are now: ${chosenCategories}`);
-
     }
 });
 
@@ -160,36 +87,13 @@ checkMeatDishes.addEventListener('click', function () {
         allDishes.value = false;
 
         dishType = "MeatDishes";
-
         console.log(`dishType: ${dishType}`);
-    //     console.log(`check is now false!`);
-
-    //     //function call to add value to array
-    //     addByValue(categoriesArr, "MeatDishes");
-
-    //     console.log(`Your chosen categories are now: ${chosenCategories}`);
-
-    // } else if (checkMeatDishes.value == "false") {
-    //     checkMeatDishes.value = "true";
-    //     console.log(`check is now true!`);
-
-    //     //function call to remove value from array. I think this can be removed.
-    //     removeByValue(categoriesArr, "Beef");
-    //     removeByValue(categoriesArr, "Chicken");
-    //     removeByValue(categoriesArr, "Lamb");
-    //     removeByValue(categoriesArr, "Pork");
-    //     removeByValue(categoriesArr, "Goat");
-        
-
-    //     chosenCategories.push("MeatDishes");
-    //     console.log(`Your chosen categories are now: ${chosenCategories}`);
-
     }
 });
 
 
 
-//MAIN SEARCH BUTTON.
+//-----------------------MAIN SEARCH BUTTON--------------------------------------------------------------------------
 buttonSearchMain.addEventListener('click', function () {
 console.log("I got clicked")
 
@@ -217,50 +121,8 @@ fetch(queryURL)
         let mealsData = data.meals;
 
         if (dishType == "allDishes") {
-            console.log("all dishes selected")
-            // //filters meals data. Not DRY but MVP.
-
-            // //VEG, MEAT & SEAFOOD
-            // if(chosenCategories.length == 3) {
-            //     console.log(`Veg, Meat Dishes + Seafood filter selected`)
-            //     mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan" || foodCategory.strCategory === "Seafood" || foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat");
-            
-            // //VEG & MEAT
-            // } 
-            // else if(chosenCategories.length == 2 && !chosenCategories.includes("Seafood")) {
-            //     console.log(`Veg + Meat filter selected`)
-            //     console.log(`ChosenCategories Array: ${chosenCategories}`)
-            //     mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan" || foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat");        
-                    
-            // //VEG & SEAFOOD
-            // } 
-            // else if(chosenCategories.length == 2 && !chosenCategories.includes("MeatDishes")) {
-            //     console.log(`Veg + Seafood filter selected`)
-            //     mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan" || foodCategory.strCategory === "Seafood");
-            
-            // //MEAT & SEAFOOD
-            // }
-            // else if(chosenCategories.length == 2 && !chosenCategories.includes("Vegetarian")) {
-            //     console.log(`Meat + Seafood filter selected`)
-            //     mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat" || foodCategory.strCategory === "Seafood"); 
-            
-
-
-            // //VEG
-           
-            // if(chosenCategories.includes("Vegetarian") == true) {
-            //     console.log(`Veg filter selected`)
-            //     mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan");
-
-            // //MEAT
-            // } else {
-            //     console.log(`Meat Dishes filter selected`)
-            //     console.log("chosenCategories arr:")
-            //     console.log(chosenCategories)
-            //     mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat" || foodCategory.strCategory === "Seafood"); 
-
-            // } 
-            
+            console.log("all dishes selected");
+    
         } else if (dishType == "Vegetarian") {
             console.log("Vegetarian Selected.");
             mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan");
@@ -270,7 +132,7 @@ fetch(queryURL)
             mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat" || foodCategory.strCategory === "Seafood"); 
 
         } else {
-            console.log("Error: check conditionals")
+            console.log("Error: check conditionals");
         }
 
 
