51b2b148816cdf5f48232f146e30e636e9e7c9d1
Orange Workbook <d.riley0189@gmail.com>
2023-12-07 20:53:52 +0000

    simplified logic working, old code commented out

--- Assets/javaScript/script.js
+++ Assets/javaScript/script.js
@@ -24,153 +24,165 @@ const uncheckAll = () => {
 
     for (var i = 0; i < w.length; i++) { 
 
-        if (w[i].type=='checkbox') { 
+        if (w[i].type=='radio') { 
 
             w[i].checked = false; 
-            
+            allDishes.checked = true;
         }
     }
 } 
 
 
-//Checkbox filters
-
+// //Checkbox filters
+let allDishes = document.getElementById("allDishes");
 let checkVegetarian = document.getElementById("vegetarian");
-let checkSeafood = document.getElementById("seafood");
 let checkMeatDishes = document.getElementById("meatDishes");
 
-let categoriesArr = [
-                        "Beef", 
-                        "Chicken", 
-                        "Dessert", 
-                        "Lamb", 
-                        "Miscellaneous", 
-                        "Pasta",
-                        "Pork",
-                        "SeaFood",
-                        "Side",
-                        "Starter",
-                        "Seafood",
-                        "Vegetarian",
-                        "Breakfast",
-                        "Goat"
-                    ];
+// let categoriesArr = [
+//                         "Beef", 
+//                         "Chicken", 
+//                         "Dessert", 
+//                         "Lamb", 
+//                         "Miscellaneous", 
+//                         "Pasta",
+//                         "Pork",
+//                         "SeaFood",
+//                         "Side",
+//                         "Starter",
+//                         "Seafood",
+//                         "Vegetarian",
+//                         "Breakfast",
+//                         "Goat"
+//                     ];
 
-let chosenCategories = [];
-let spliceByIndex;
+// let chosenCategories = [];
+// let spliceByIndex;
 
-//function to remove an element from an array based on its name
-const removeByValue = (arr, value) => {
-    let indexOf = arr.indexOf(value);
+// //function to remove an element from an array based on its name
+// const removeByValue = (arr, value) => {
+//     let indexOf = arr.indexOf(value);
 
-    for(i=0; i<arr.length; i++) {
-        if(value == arr[i]) {
-            //console.log(`value: ${value} arr[i]: ${arr[i]}`);
-            spliceByIndex = arr.splice(indexOf, 1);
-            //console.log(`Spliced value: ${spliceByIndex}`);
+//     for(i=0; i<arr.length; i++) {
+//         if(value == arr[i]) {
+//             //console.log(`value: ${value} arr[i]: ${arr[i]}`);
+//             spliceByIndex = arr.splice(indexOf, 1);
+//             //console.log(`Spliced value: ${spliceByIndex}`);
 
-        }
-    }
+//         }
+//     }
     
-    //console.log(`Array values: ${arr}`);
-    return arr   
-};
+//     //console.log(`Array values: ${arr}`);
+//     return arr   
+// };
 
-//function to add an element to an array
-const addByValue = (arr, value) => {
+// //function to add an element to an array
+// const addByValue = (arr, value) => {
 
-    //checking value is not already in array before pushing
-    if(arr.includes(value) != true) {
-        arr.push(value);
-        //console.log(`Pushed value: ${value}`);
+//     //checking value is not already in array before pushing
+//     if(arr.includes(value) != true) {
+//         arr.push(value);
+//         //console.log(`Pushed value: ${value}`);
 
-    }
+//     }
 
-    //removes element from array of chosen categories
-    removeByValue(chosenCategories, value);
+//     //removes element from array of chosen categories
+//     removeByValue(chosenCategories, value);
     
 
-    //console.log(`Your chosen categories are now: ${chosenCategories}`);
-    //console.log(`Array values: ${arr}`);
-    return arr   
-};
+//     //console.log(`Your chosen categories are now: ${chosenCategories}`);
+//     //console.log(`Array values: ${arr}`);
+//     return arr   
+// };
+
+let dishType = "allDishes";
 
 //--------------------------------------EVENT LISTENERS------------------------------------------------
 
 //TODO: SAVE ALL CHECKBOXES TO VARIABLE. EVENT LISTENER SHOULD USE IF STATEMENTS TO SELECT CORRECT ID.
 
 //checkbox eventListener
+
+allDishes.addEventListener('click', function (event) {
+
+    console.log(event.target.value)
+    if(allDishes.value == "false") {
+    
+        allDishes.value = true;
+        checkVegetarian.value = false;
+        checkMeatDishes.value = false;
+        
+        dishType = "allDishes";
+        console.log(`dishType: ${dishType}`);
+
+    }
+});
+
 //Vegetarian only. Not DRY but for MVP purposes.
-checkVegetarian.addEventListener('click', function () {
+checkVegetarian.addEventListener('click', function (event) {
 
-    if(checkVegetarian.value == "true") {
-        checkVegetarian.value = "false";
-        console.log(`check is now false!`);
+    console.log(event.target.value)
+    if(checkVegetarian.value == "false") {
+    
+        checkVegetarian.value = true;
+        checkMeatDishes.value = false;
+        allDishes.value = false;
+        
+        dishType = "Vegetarian";
+        console.log(`dishType: ${dishType}`);
+    //     checkVegetarian.value = "false";
+    //     console.log(`check is now false!`);
 
-        //function call to add value to array
-        addByValue(categoriesArr, "Vegetarian");
+    //     //function call to add value to array
+    //     addByValue(categoriesArr, "Vegetarian");
         
 
-    } else if (checkVegetarian.value == "false") {
-        checkVegetarian.value = "true";
-        console.log(`check is now true!`);
+    // } else if (checkVegetarian.value == "false") {
+    //     checkVegetarian.value = "true";
+    //     console.log(`check is now true!`);
 
-        //function call to remove value from array
-        removeByValue(categoriesArr, "Vegetarian");
+    //     //function call to remove value from array
+    //     removeByValue(categoriesArr, "Vegetarian");
 
-        chosenCategories.push(spliceByIndex);
-        console.log(`Your chosen categories are now: ${chosenCategories}`);
-
-    }
-});
-
-//Seafood only. Not DRY but for MVP purposes.
-checkSeafood.addEventListener('click', function () {
-
-    if(checkSeafood.value == "true") {
-        checkSeafood.value = "false";
-        console.log(`check is now false!`);
-
-        //function call to add value to array
-        addByValue(categoriesArr, "Seafood");
-
-    } else if (checkSeafood.value == "false") {
-        checkSeafood.value = "true";
-        console.log(`check is now true!`);
-
-        //function call to remove value from array
-        removeByValue(categoriesArr, "Seafood");
-
-        chosenCategories.push(spliceByIndex);
-        console.log(`Your chosen categories are now: ${chosenCategories}`);
+    //     chosenCategories.push(spliceByIndex);
+    //     console.log(`Your chosen categories are now: ${chosenCategories}`);
 
     }
 });
 
-//Seafood only. Not DRY but for MVP purposes.
+
+//MeatDishes only. Not DRY but for MVP purposes.
 checkMeatDishes.addEventListener('click', function () {
 
-    if(checkMeatDishes.value == "true") {
-        checkMeatDishes.value = "false";
-        console.log(`check is now false!`);
+    if(checkMeatDishes.value == "false") {
 
-        //function call to add value to array
-        addByValue(categoriesArr, "MeatDishes");
+        checkMeatDishes.value = true;
+        checkVegetarian.value = false;
+        allDishes.value = false;
 
-    } else if (checkMeatDishes.value == "false") {
-        checkMeatDishes.value = "true";
-        console.log(`check is now true!`);
+        dishType = "MeatDishes";
 
-        //function call to remove value from array. I think this can be removed.
-        removeByValue(categoriesArr, "Beef");
-        removeByValue(categoriesArr, "Chicken");
-        removeByValue(categoriesArr, "Lamb");
-        removeByValue(categoriesArr, "Pork");
-        removeByValue(categoriesArr, "Goat");
+        console.log(`dishType: ${dishType}`);
+    //     console.log(`check is now false!`);
+
+    //     //function call to add value to array
+    //     addByValue(categoriesArr, "MeatDishes");
+
+    //     console.log(`Your chosen categories are now: ${chosenCategories}`);
+
+    // } else if (checkMeatDishes.value == "false") {
+    //     checkMeatDishes.value = "true";
+    //     console.log(`check is now true!`);
+
+    //     //function call to remove value from array. I think this can be removed.
+    //     removeByValue(categoriesArr, "Beef");
+    //     removeByValue(categoriesArr, "Chicken");
+    //     removeByValue(categoriesArr, "Lamb");
+    //     removeByValue(categoriesArr, "Pork");
+    //     removeByValue(categoriesArr, "Goat");
         
 
-        chosenCategories.push("MeatDishes");
-        console.log(`Your chosen categories are now: ${chosenCategories}`);
+    //     chosenCategories.push("MeatDishes");
+    //     console.log(`Your chosen categories are now: ${chosenCategories}`);
 
     }
 });
@@ -179,6 +191,7 @@ checkMeatDishes.addEventListener('click', function () {
 
 //MAIN SEARCH BUTTON.
 buttonSearchMain.addEventListener('click', function () {
+console.log("I got clicked")
 
 document.getElementById('main-content').setAttribute('class', 'show');
 
@@ -203,106 +216,64 @@ fetch(queryURL)
 
         let mealsData = data.meals;
 
-        //console.log(chosenCategories)
+        if (dishType == "allDishes") {
+            console.log("all dishes selected")
+            // //filters meals data. Not DRY but MVP.
 
-        if (chosenCategories.length > 0) {
-
-            //filters meals data. Not DRY but MVP.
-
-            //VEG, MEAT & SEAFOOD
-            if(chosenCategories.length == 3) {
-                console.log(`Veg, Meat Dishes + Seafood filter selected`)
-                mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan" || foodCategory.strCategory === "Seafood" || foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat");
+            // //VEG, MEAT & SEAFOOD
+            // if(chosenCategories.length == 3) {
+            //     console.log(`Veg, Meat Dishes + Seafood filter selected`)
+            //     mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan" || foodCategory.strCategory === "Seafood" || foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat");
             
-            }
-//TODO: ADD SYNTAX TO ALL 3 2 CHECKLIST OPTIONS
-            if(chosenCategories.length == 2 && !chosenCategories.includes("Vegetarian")) {
-
-
-                console.log("seafood + meat test")
-                for(i=0; i<chosenCategories.length; i++){
-                    let j = 0;
-                    //VEG & MEAT
-                    if(chosenCategories[i] != "Seafood") {
-                        console.log("how many times")
-                        j++
-                        if(j == 2) {
-                            console.log(`Veg + Meat filter selected`)
-                            mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan" || foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat");
-                        }
-                    console.log(`Meat + Veg J:${j}`)
-                        
+            // //VEG & MEAT
+            // } 
+            // else if(chosenCategories.length == 2 && !chosenCategories.includes("Seafood")) {
+            //     console.log(`Veg + Meat filter selected`)
+            //     console.log(`ChosenCategories Array: ${chosenCategories}`)
+            //     mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan" || foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat");        
                     
-                    //VEG & SEAFOOD
-                    } 
+            // //VEG & SEAFOOD
+            // } 
+            // else if(chosenCategories.length == 2 && !chosenCategories.includes("MeatDishes")) {
+            //     console.log(`Veg + Seafood filter selected`)
+            //     mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan" || foodCategory.strCategory === "Seafood");
+            
+            // //MEAT & SEAFOOD
+            // }
+            // else if(chosenCategories.length == 2 && !chosenCategories.includes("Vegetarian")) {
+            //     console.log(`Meat + Seafood filter selected`)
+            //     mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat" || foodCategory.strCategory === "Seafood"); 
+            
 
-                    j = 0;
 
-                    if(chosenCategories[i] != "MeatDishes") {
-                        j++
-                        if(j == 2) {
-                            console.log(`Veg + Seafood filter selected`)
-                            mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan" || foodCategory.strCategory === "Seafood");
-                        }
-                    console.log(`Seafood + Veg J:${j}`)
+            // //VEG
+           
+            // if(chosenCategories.includes("Vegetarian") == true) {
+            //     console.log(`Veg filter selected`)
+            //     mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan");
 
-                    //MEAT & SEAFOOD    
-                    } 
-                    
-                    j = 0;
+            // //MEAT
+            // } else {
+            //     console.log(`Meat Dishes filter selected`)
+            //     console.log("chosenCategories arr:")
+            //     console.log(chosenCategories)
+            //     mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat" || foodCategory.strCategory === "Seafood"); 
 
-                    if(chosenCategories[i] != "Vegetarian") {
-                        j++
-                        if(j == 2) {
-                        console.log(`Meat + Seafood filter selected`)
-                        mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat" || foodCategory.strCategory === "Seafood"); 
-                        }
-                    console.log(`Seafood + Meat J:${j}`)
-                    
-                    }
-                }
-                // //VEG & MEAT
-                // if(chosenCategories.includes("Seafood") == false) {
-                //     console.log(`Veg + Meat filter selected`)
-                //     mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan" || foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat");
-                
-                // console.log(chosenCategories)
-                // //VEG & SEAFOOD
-                // } else if(chosenCategories.includes("MeatDishes") == false) {
-                //     console.log(`Veg + Seafood filter selected`)
-                //     mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan" || foodCategory.strCategory === "Seafood");
+            // } 
+            
+        } else if (dishType == "Vegetarian") {
+            console.log("Vegetarian Selected.");
+            mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan");
 
-                // //MEAT & SEAFOOD
-                // } else if(chosenCategories.includes("Vegetarian") == false) {
-                //     console.log(`Meat Dishes + Seafood filter selected`)
-                //     mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat" || foodCategory.strCategory === "Seafood"); 
-                // }
-            }
-            //VEG
-            if(chosenCategories.includes("Vegetarian") == true && chosenCategories.includes("MeatDishes") == false  && chosenCategories.includes("Seafood") == false) {
-                console.log(`Veg filter selected`)
-                mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Vegetarian" || foodCategory.strCategory === "Vegan");
+        } else if (dishType == "MeatDishes") {
+            console.log("Meat Dishes Selected");
+            mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat" || foodCategory.strCategory === "Seafood"); 
 
-            //MEAT
-            } else if(chosenCategories.includes("MeatDishes") == true && chosenCategories.includes("Vegetarian") == false && chosenCategories.includes("Seafood") == false) {
-                console.log(`Meat Dishes filter selected`)
-                console.log("chosenCategories arr:")
-                console.log(chosenCategories)
-                mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Chicken" || foodCategory.strCategory === "Lamb" || foodCategory.strCategory === "Pork" || foodCategory.strCategory === "Goat"); 
-
-            //SEAFOOD
-            } else if(chosenCategories.includes("Seafood") == true && chosenCategories.includes("Vegetarian") == false && chosenCategories.includes("MeatDishes") == false) {
-                console.log(`Seafood filter selected`)
-                mealsData = data.meals.filter(foodCategory => foodCategory.strCategory === "Seafood");
-
-            } else {
-                console.log("error: check logic on filters.")
-            }
-
+        } else {
+            console.log("Error: check conditionals")
         }
 
-        //const meatCategorary = data.meals.filter(foodCategory => foodCategory.strCategory === "Beef" || foodCategory.strCategory === "Pork");
-        //Function to pull a different recipe if more than 1 is returned from API.
+
         function getRandomArray(array) {
             for (i = 0; i < data.meals.length; i++) {
 
