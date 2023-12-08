9e097db5dd44ff6aab991ad4741940e0ffa8d8ad
tosin-t <tosin100@homail.com>
2023-12-08 17:06:14 +0000

    m

--- Assets/javaScript/recipe.js
+++ Assets/javaScript/recipe.js
@@ -1,371 +1,255 @@
 var buttonSearchMain = document.getElementById('main-btn');
 var buttonSearchSecondary = document.getElementById('secondary-btn');
 var mainSeachBar = document.getElementById('main-search');
-var favouriteButton=document.getElementById("favouriteButton")
-var favourites= document.getElementsByClassName("recipies")
+var favouriteButton = document.getElementById("favouriteButton");
 var userSearchMain = "";
 var userSearchSecondary = "";
-var dishObject={}
-var newFavouriteButton= document.createElement("button")
+var dishObject = {};
+var newFavouriteButton = document.createElement("button");
 
-
-
-var apiURL = "https://www.themealdb.com/api/json/v1/1/search.php?s="
-var y = "https://www.themealdb.com/api/json/v1/1/random.php"
+var apiURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+var y = "https://www.themealdb.com/api/json/v1/1/random.php";
 var queryURL;
 
+var favouriteRecipe = [];
+var favouriteRecipeListID = document.getElementById("favoritesList");
+// Load favourite buttons on loading
+document.addEventListener("DOMContentLoaded", LoadingFavourite);
 
-// load favourite buttons on loading 
-document.addEventListener("DOMContentLoaded", LoadingFavourite)
-
-function LoadingFavourite(){
-    var storedFavourites = localStorage.getItem("Favourite Recipes")
-    if (storedFavourites){
-favouriteRecipe=JSON.parse(storedFavourites)
-        if (favouriteRecipe.length>0){
-            renderFavouriteRecipeButtonpreload()
-        }
-
+function LoadingFavourite() {
+  var storedFavourites = localStorage.getItem("Favourite Recipes");
+  if (storedFavourites) {
+    favouriteRecipe = JSON.parse(storedFavourites);
+    if (favouriteRecipe.length > 0) {
+      renderFavouriteRecipeButtonpreload();
     }
+  }
 }
-// Fav button function
-newFavouriteButton.addEventListener("click", callFavourite)
-//MAIN SEARCH BUTTON.
 
+// Fav button function
+newFavouriteButton.addEventListener("click", callFavourite);
+
+// MAIN SEARCH BUTTON.
 buttonSearchMain.addEventListener('click', PresentContent);
 
+function PresentContent() {
+  document.getElementById('main-content').setAttribute('class', 'show');
+  console.log('main button works');
+  mainSeachBar.setAttribute('class', 'hide');
+  var userSearchMain = document.getElementById('mainInput').value;
+  console.log(userSearchMain);
 
-    function PresentContent() {
-        
-    
-    document.getElementById('main-content').setAttribute('class', 'show');
+  queryURL = apiURL + userSearchMain;
 
-    console.log('main button works');
-    mainSeachBar.setAttribute('class', 'hide');
-    var userSearchMain = document.getElementById('mainInput').value;
-    console.log(userSearchMain);
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      function getRandomArray(array) {
+        for (i = 0; i < data.meals.length; i++) {
+          var randomArray = Math.floor(Math.random() * array.length);
+          return array[randomArray];
+        }
+      }
 
-    
+      var dish = getRandomArray(data.meals);
 
-    queryURL = apiURL + userSearchMain;
+      dishObject.id = dish.idMeal;
+      dishObject.recipeName = dish.strMeal;
 
+      document.getElementById("instructions").textContent = dish.strInstructions;
+      var RandomMealname = document.getElementById("recipe-name").textContent = dish.strMeal;
+      document.getElementById("image").setAttribute('src', dish.strMealThumb);
 
-
-    fetch(queryURL)
-        .then(function (response) {
-            return response.json();
-        }).then(function (data) {
-
-            //Function to pull a different recipe if more than 1 is returned from API.
-            function getRandomArray(array) {
-                for (i = 0; i < data.meals.length; i++) {
-                    var randomArray = Math.floor(Math.random() * array.length);
-                    return array[randomArray];
-                }
-            }
-           
-            var dish = getRandomArray(data.meals);
-
-            dishObject.id= dish.idMeal,
-            dishObject.recipeName=dish.strMeal
-            
-console.log(dishObject)
-
-
-
-            document.getElementById("instructions").textContent = dish.strInstructions;;
-            var RandomMealname=document.getElementById("recipe-name").textContent = dish.strMeal;
-            document.getElementById("image").setAttribute('src', dish.strMealThumb);
-
-
-console.log(data)
-
-            //Function to get ingredients.
-            var ingredientsArray = Object.keys(dish)
-                .filter(key => key.startsWith('strIngredient'))
-                .map(key => dish[key])
-                .filter(ingredient => ingredient !== '')
-                .filter(ingredient => ingredient !== null);
-            
-
-            for (var i = 0; i < ingredientsArray.length; i++) {
-
-                var ListEl = document.querySelector('#ingredients');
-
-                var ingredient = ingredientsArray[i];
-
-
-                var li = document.createElement("li");
-                li.textContent = ingredient;
-                li.setAttribute('class', 'ingredients');
-                ListEl.appendChild(li);
-
-
-            }
-
-            //Function to get measures.
-            var measureArray = Object.keys(dish)
-            .filter(key => key.startsWith('strMeasure'))
-            .map(key => dish[key])
-            .filter(measure =>measure !=='')
-            .filter(measure => measure !== null);
-
-
-            for (var x = 0; x < measureArray.length; x++) {
-                
-                var ListElementMeasure =document.querySelector ('#measure');
-                var measure = measureArray[x];
-                var li2 = document.createElement("li");
-                li2.textContent = measure;
-                li2.setAttribute('class', 'measure');
-                ListElementMeasure.appendChild(li2);
-
-
-
-
-            }
-
-
-
-
-
-        })
-
-
-
-
-
-}; 
- function displayCopy(){
-    var dish = data.meals;
-
-    document.getElementById("instructions").textContent = dish.strInstructions;;
-    var RandomMealname=document.getElementById("recipe-name").textContent = dish.strMeal;
-    document.getElementById("image").setAttribute('src', dish.strMealThumb);
-
-
-console.log(data)
-
-    //Function to get ingredients.
-    var ingredientsArray = Object.keys(dish)
+      var ingredientsArray = Object.keys(dish)
         .filter(key => key.startsWith('strIngredient'))
         .map(key => dish[key])
         .filter(ingredient => ingredient !== '')
         .filter(ingredient => ingredient !== null);
-    
 
-    for (var i = 0; i < ingredientsArray.length; i++) {
-
+      for (var i = 0; i < ingredientsArray.length; i++) {
         var ListEl = document.querySelector('#ingredients');
-
         var ingredient = ingredientsArray[i];
-
-
         var li = document.createElement("li");
         li.textContent = ingredient;
         li.setAttribute('class', 'ingredients');
         ListEl.appendChild(li);
+      }
 
+      var measureArray = Object.keys(dish)
+        .filter(key => key.startsWith('strMeasure'))
+        .map(key => dish[key])
+        .filter(measure => measure !== '')
+        .filter(measure => measure !== null);
 
-    }
-
-    //Function to get measures.
-    var measureArray = Object.keys(dish)
-    .filter(key => key.startsWith('strMeasure'))
-    .map(key => dish[key])
-    .filter(measure =>measure !=='')
-    .filter(measure => measure !== null);
-
-
-    for (var x = 0; x < measureArray.length; x++) {
-        
-        var ListElementMeasure =document.querySelector ('#measure');
+      for (var x = 0; x < measureArray.length; x++) {
+        var ListElementMeasure = document.querySelector('#measure');
         var measure = measureArray[x];
         var li2 = document.createElement("li");
         li2.textContent = measure;
         li2.setAttribute('class', 'measure');
         ListElementMeasure.appendChild(li2);
-
-
-
-
-    }
-
-
-
-
-
+      }
+    });
 }
 
-
-
- 
-
-
-
-// create button feature
-var favouriteRecipe=[]
-var favouriteRecipeListID= document.getElementById("favoritesList")
-
-function renderFavouriteRecipeButton(){
-    favouriteRecipe.push(dishObject)
-localStorage.setItem("Favourite Recipes", JSON.stringify(favouriteRecipe))    
-console.log(favouriteRecipe)
-favouriteRecipeListID.innerHTML=''
-
-for (i=0;i<favouriteRecipe.length;i++){
-    const recipe=favouriteRecipe[i]
-    console.log(recipe)
-    var favouriteButton= document.createElement("button")
-favouriteButton.classList.add("recipies")
-favouriteButton.setAttribute("data-recipeId",recipe.id);
-favouriteButton.textContent=recipe.recipeName
-favouriteRecipeListID.append(favouriteButton)
-}
-}
-
-function renderFavouriteRecipeButtonpreload(){
-    
-localStorage.setItem("Favourite Recipes", JSON.stringify(favouriteRecipe))    
-console.log(favouriteRecipe)
-favouriteRecipeListID.innerHTML=''
-
-for (i=0;i<favouriteRecipe.length;i++){
-    const recipe=favouriteRecipe[i]
-    console.log(recipe)
-    var newFavouriteButton= document.createElement("button")
-newFavouriteButton.classList.add("recipies")
-newFavouriteButton.setAttribute("data-recipeId",recipe.id);
-newFavouriteButton.textContent=recipe.recipeName
-favouriteRecipeListID.append(newFavouriteButton)
-
-newFavouriteButton.addEventListener("click", callFavourite)
-}
-}
-// buttonSearchMain.addEventListener("click",test )
-favouriteButton.addEventListener('click', function() {
-    renderFavouriteRecipeButton();
+// SEARCH BUTTON ON THE TOP RIGHT-HAND CORNER.
+buttonSearchSecondary.addEventListener('click', function (event) {
+  event.preventDefault();
+  PresentContent();
+  console.log('secondary button works');
 });
 
+// Create button feature
+var favouriteRecipe = [];
 
+function renderFavouriteRecipeButton() {
+  if (!localStorage.getItem("Favourite Recipes")) {
+    localStorage.setItem("Favourite Recipes", JSON.stringify([]));
+  }
 
-    // GET RANDOM MEAL BUTTON
+  var storedFavourites = JSON.parse(localStorage.getItem("Favourite Recipes"));
 
-var getRandomMeal= document.getElementById("get-meal"); 
+  storedFavourites.push(dishObject);
+  localStorage.setItem("Favourite Recipes", JSON.stringify(storedFavourites));
+
+  favouriteRecipeListID.innerHTML = '';
+
+  for (i = 0; i < storedFavourites.length; i++) {
+    const recipe = storedFavourites[i];
+    console.log(recipe);
+    var favouriteButton = document.createElement("button");
+    favouriteButton.classList.add("recipies");
+    favouriteButton.setAttribute("data-recipeId", recipe.id);
+    favouriteButton.textContent = recipe.recipeName;
+    favouriteRecipeListID.append(favouriteButton);
+
+    favouriteButton.addEventListener("click", callFavourite);
+  }
+}
+
+function renderFavouriteRecipeButtonpreload() {
+  localStorage.setItem("Favourite Recipes", JSON.stringify(favouriteRecipe));
+  console.log(favouriteRecipe);
+  favouriteRecipeListID.innerHTML = '';
+
+  for (i = 0; i < favouriteRecipe.length; i++) {
+    const recipe = favouriteRecipe[i];
+    console.log(recipe);
+    var newFavouriteButton = document.createElement("button");
+    newFavouriteButton.classList.add("recipies");
+    newFavouriteButton.setAttribute("data-recipeId", recipe.id);
+    newFavouriteButton.textContent = recipe.recipeName;
+    favouriteRecipeListID.append(newFavouriteButton);
+
+    newFavouriteButton.addEventListener("click", callFavourite);
+  }
+}
+
+// Fav button function
+favouriteButton.addEventListener('click', function () {
+  renderFavouriteRecipeButton();
+});
+
+// GET RANDOM MEAL BUTTON
+var getRandomMeal = document.getElementById("get-meal");
 
 getRandomMeal.addEventListener('click', function (event) {
-    event.preventDefault();
-    mainSeachBar.setAttribute('class', 'hide');
+  event.preventDefault();
+  mainSeachBar.setAttribute('class', 'hide');
 
+  document.getElementById('main-content').setAttribute('class', 'show');
+  fetch(y)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var dish = data.meals[0];
+      console.log(dish);
+      document.getElementById("instructions").textContent = dish.strInstructions;
+      document.getElementById("recipe-name").textContent = dish.strMeal;
+      document.getElementById("image").setAttribute('src', dish.strMealThumb);
 
-document.getElementById('main-content').setAttribute('class', 'show');
-    fetch(y)
-        .then(function (response) {
-            return response.json();
-        }).then(function (data) {
+      var ingredientsArray = Object.keys(dish)
+        .filter(key => key.startsWith('strIngredient'))
+        .map(key => dish[key])
+        .filter(ingredient => ingredient !== '')
+        .filter(ingredient => ingredient !== null);
 
-            var dish = data.meals[0];
+      for (var i = 0; i < ingredientsArray.length; i++) {
+        var ListEl = document.querySelector('#ingredients');
+        var ingredient = ingredientsArray[i];
+        var li = document.createElement("li");
+        li.textContent = ingredient;
+        li.setAttribute('class', 'ingredients');
+        ListEl.appendChild(li);
+      }
 
-            console.log(dish);
-            document.getElementById("instructions").textContent = dish.strInstructions;
-            document.getElementById("recipe-name").textContent = dish.strMeal;
-            document.getElementById("image").setAttribute('src', dish.strMealThumb);
-           
-            //Function to get ingredients.
-            var ingredientsArray = Object.keys(dish)
-                .filter(key => key.startsWith('strIngredient'))
-                .map(key => dish[key])
-                .filter(ingredient => ingredient !== '')
-                .filter(ingredient => ingredient !== null);
-            
+      var measureArray = Object.keys(dish)
+        .filter(key => key.startsWith('strMeasure'))
+        .map(key => dish[key])
+        .filter(measure => measure !== '')
+        .filter(measure => measure !== null);
 
-            for (var i = 0; i < ingredientsArray.length; i++) {
+      for (var x = 0; x < measureArray.length; x++) {
+        var ListElementMeasure = document.querySelector('#measure');
+        var measure = measureArray[x];
+        var li2 = document.createElement("li");
+        li2.textContent = measure;
+        li2.setAttribute('class', 'measure');
+        ListElementMeasure.appendChild(li2);
+      }
+    });
+});
 
-                var ListEl = document.querySelector('#ingredients');
+function callFavourite() {
+  var mealID = this.getAttribute("data-recipeId");
+  var querryurlSearch = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
 
-                var ingredient = ingredientsArray[i];
+  var querryurlcallback = querryurlSearch + mealID;
+  console.log(querryurlcallback);
 
+  fetch(querryurlcallback)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var dish = data.meals[0];
+      console.log(dish);
+      document.getElementById("instructions").textContent = dish.strInstructions;
+      document.getElementById("recipe-name").textContent = dish.strMeal;
+      document.getElementById("image").setAttribute('src', dish.strMealThumb);
 
-                var li = document.createElement("li");
-                li.textContent = ingredient;
-                li.setAttribute('class', 'ingredients');
-                ListEl.appendChild(li);
+      var ingredientsArray = Object.keys(dish)
+        .filter(key => key.startsWith('strIngredient'))
+        .map(key => dish[key])
+        .filter(ingredient => ingredient !== '')
+        .filter(ingredient => ingredient !== null);
 
+      for (var i = 0; i < ingredientsArray.length; i++) {
+        var ListEl = document.querySelector('#ingredients');
+        var ingredient = ingredientsArray[i];
+        var li = document.createElement("li");
+        li.textContent = ingredient;
+        li.setAttribute('class', 'ingredients');
+        ListEl.appendChild(li);
+      }
 
-            }
+      var measureArray = Object.keys(dish)
+        .filter(key => key.startsWith('strMeasure'))
+        .map(key => dish[key])
+        .filter(measure => measure !== '')
+        .filter(measure => measure !== null);
 
-            //Function to get measures.
-            var measureArray = Object.keys(dish)
-            .filter(key => key.startsWith('strMeasure'))
-            .map(key => dish[key])
-            .filter(measure =>measure !=='')
-            .filter(measure => measure !== null);
-
-
-            for (var x = 0; x < measureArray.length; x++) {
-                
-                var ListElementMeasure =document.querySelector ('#measure');
-                var measure = measureArray[x];
-                var li2 = document.createElement("li");
-                li2.textContent = measure;
-                li2.setAttribute('class', 'measure');
-                ListElementMeasure.appendChild(li2);
-
-
-
-
-            }
-
-
-        
-            
-        })
- });
-
-function callFavourite(){
-  
-    var mealID= this.getAttribute("data-recipeId")
-    var querryurlSearch="https://www.themealdb.com/api/json/v1/1/lookup.php?i="
-    var querryurlcallback=querryurlSearch+mealID
-    console.log(querryurlcallback)
-    fetch(querryurlcallback)
-           .then(function (response) {
-               return response.json();
-           }).then(function (data) {
-            console.log(data) 
-            var dish = data.meals[0];
-            console.log(dish);
-            document.getElementById("instructions").textContent = dish.strInstructions;
-            document.getElementById("recipe-name").textContent = dish.strMeal;
-            document.getElementById("image").setAttribute('src', dish.strMealThumb);
-            //Function to get ingredients.
-            var ingredientsArray = Object.keys(dish)
-                .filter(key => key.startsWith('strIngredient'))
-                .map(key => dish[key])
-                .filter(ingredient => ingredient !== '')
-                .filter(ingredient => ingredient !== null);
-            for (var i = 0; i < ingredientsArray.length; i++) {
-                var ListEl = document.querySelector('#ingredients');
-                var ingredient = ingredientsArray[i];
-                var li = document.createElement("li");
-                li.textContent = ingredient;
-                li.setAttribute('class', 'ingredients');
-                ListEl.appendChild(li);
-            }
-            //Function to get measures.
-            var measureArray = Object.keys(dish)
-            .filter(key => key.startsWith('strMeasure'))
-            .map(key => dish[key])
-            .filter(measure =>measure !=='')
-            .filter(measure => measure !== null);
-            for (var x = 0; x < measureArray.length; x++) {
-                var ListElementMeasure =document.querySelector ('#measure');
-                var measure = measureArray[x];
-                var li2 = document.createElement("li");
-                li2.textContent = measure;
-                li2.setAttribute('class', 'measure');
-                ListElementMeasure.appendChild(li2);
-            }
-        })
-           }
-           
+      for (var x = 0; x < measureArray.length; x++) {
+        var ListElementMeasure = document.querySelector('#measure');
+        var measure = measureArray[x];
+        var li2 = document.createElement("li");
+        li2.textContent = measure;
+        li2.setAttribute('class', 'measure');
+        ListElementMeasure.appendChild(li2);
+      }
+    });
+}
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# recipe-generator
+
+# Recipe Generator
+
+This is a Food recipe generator that uses prompts from users to direct its search into a API data base and display a picture, ingredients and instructions. It also provides a random fact using a second API should the user want to. The goal of this project stemmed from the stress that comes with deciding what to eat when there a limited products at home
+
+## Installation
+
+This project can be downloaded from GitHub.
+
+To download the content, go to the portfolio page which can be found here: https://github.com/Tosin-T/recipe-generator/
+
+From this page to download the content go to the code button tab. click the link and below should be a drop down menu with "Download Zip."
+
+Press this and the file will begin to download to a file location of your choice.
+
+ONCE downloaded, unzip the file using your desired software.
+
+Next go to the index.html and open in your browser. .
+
+
+## Usage
+
+1) Open webpage
+2) Enter desired food item into search, using the check boxes to filter for food requirements
+3) Click go to execute search from API
+4) If recipe is not to user liking locate search again and repeat step 2+3
+5) If the user see a recipe they would like to save for later, locate the favourite button and click it to save desired recipe to sidebar
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
